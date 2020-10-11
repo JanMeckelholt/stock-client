f8858b129cabd38b008d1a4c0edc61b2ac16ca88
JanMeckelholt <Jan_Meckelholt@web.de>
2020-10-11 07:33:44 +0200

    after Relocation of folder

---
+++ src/main/java/de/kfru/stockclient/StockPrice.java
@@ -0,0 +1,18 @@
+package de.kfru.stockclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockPrice {
+    private String symbol;
+    private Double price;
+    private LocalDateTime time;
+
+
+}
